b34f7bda85ccd672c752121df71b1f2efe3cb411
Bobo <Bobo@BOBO-GAME>
2022-06-30 19:29:37 +0200

    Config extended

---
+++ Persistence/SpecialRewardSettings.cs
@@ -0,0 +1,63 @@
+﻿using System.Xml.Serialization;
+
+namespace Los.Santos.Dope.Wars.Persistence
+{
+	/// <summary>
+	/// The <see cref="SpecialRewardSettings"/> class is the root element for the special rewards settings 
+	/// </summary>
+	[XmlRoot(ElementName = nameof(SpecialRewardSettings), IsNullable = false)]
+	public class SpecialRewardSettings
+	{
+		#region ctor
+		/// <summary>
+		/// The <see cref="SpecialRewardSettings"/> standard constructor with standard values
+		/// </summary>
+		public SpecialRewardSettings()
+		{
+			Warehouse = new();
+		}
+		#endregion
+
+		#region properties
+		/// <summary>
+		/// The <see cref="Warehouse"/> property
+		/// </summary>
+		[XmlElement(ElementName = nameof(WarehouseSettings), IsNullable = false)]
+		public WarehouseSettings Warehouse { get; set; }
+		#endregion
+
+		#region sub classes
+		/// <summary>
+		/// The <see cref="WarehouseSettings"/> class is the <see cref="WarehouseSettings"/> element for the special rewards settings
+		/// </summary>
+		[XmlRoot(ElementName = nameof(WarehouseSettings), IsNullable = false)]
+		public class WarehouseSettings
+		{
+			#region ctor
+			/// <summary>
+			/// The <see cref="Warehouse"/> standard constructor with standard values
+			/// </summary>
+			public WarehouseSettings()
+			{
+				WarehousePrice = 125000;
+				WarehouseUpgradePrice = 875000;
+			}
+			#endregion
+
+			#region properties
+			/// <summary>
+			/// The <see cref="WarehousePrice"/> property
+			/// </summary>
+			[XmlAttribute(AttributeName = nameof(WarehousePrice))]
+			public int WarehousePrice { get; set; }
+
+			/// <summary>
+			/// The <see cref="WarehouseUpgradePrice"/> property
+			/// </summary>
+			[XmlAttribute(AttributeName = nameof(WarehouseUpgradePrice))]
+			public int WarehouseUpgradePrice { get; set; }
+			#endregion
+		}
+		#endregion
+	}
+}
