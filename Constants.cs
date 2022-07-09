65f601a0b5bca39a8b9b03163c42c08628cea771
Bobo <Bobo@BOBO-GAME>
2022-07-09 20:19:56 +0200

    const clean up

--- Constants.cs
+++ Constants.cs
@@ -76,136 +76,118 @@ namespace Los.Santos.Dope.Wars
 		/// The <see cref="DrugDealerPedHashes"/> list holds all the possible dealer models
 		/// </summary>
 		public static readonly List<PedHash> DrugDealerPedHashes = new()
-				{
-						PedHash.Famdd01,
-						PedHash.Famca01GMY,
-						PedHash.Famdnf01GMY,
-						PedHash.Famfor01GMY,
-						PedHash.Lost01GMY,
-						PedHash.Lost02GMY,
-						PedHash.Lost03GMY,
-						PedHash.BallaEast01GMY,
-						PedHash.BallaOrig01GMY,
-						PedHash.Ballasog,
-						PedHash.MexGang01GMY,
-						PedHash.MexGoon01GMY,
-						PedHash.MexThug01AMY,
-						PedHash.Korean01GMY,
-						PedHash.Korean02GMY,
-						PedHash.PoloGoon01GMY,
-						PedHash.PoloGoon02GMY,
-						PedHash.SalvaGoon01GMY,
-						PedHash.SalvaGoon02GMY,
-						PedHash.SalvaGoon03GMY,
-						PedHash.ChiGoon01GMM,
-						PedHash.ChiGoon02GMM,
-						PedHash.ArmGoon02GMY
-				};
+		{
+			PedHash.Famdd01,
+			PedHash.Famca01GMY,
+			PedHash.Famdnf01GMY,
+			PedHash.Famfor01GMY,
+			PedHash.Lost01GMY,
+			PedHash.Lost02GMY,
+			PedHash.Lost03GMY,
+			PedHash.BallaEast01GMY,
+			PedHash.BallaOrig01GMY,
+			PedHash.Ballasog,
+			PedHash.MexGang01GMY,
+			PedHash.MexGoon01GMY,
+			PedHash.MexThug01AMY,
+			PedHash.Korean01GMY,
+			PedHash.Korean02GMY,
+			PedHash.PoloGoon01GMY,
+			PedHash.PoloGoon02GMY,
+			PedHash.SalvaGoon01GMY,
+			PedHash.SalvaGoon02GMY,
+			PedHash.SalvaGoon03GMY,
+			PedHash.ChiGoon01GMM,
+			PedHash.ChiGoon02GMM,
+			PedHash.ArmGoon02GMY
+		};
 
+		/// <summary>
+		/// The <see cref="DrugLordPedHashes"/> list holds all the possible drug lord models
+		/// </summary>
 		public static readonly List<PedHash> DrugLordPedHashes = new()
-				{
-						PedHash.ChiBoss01GMM,
-						PedHash.KorBoss01GMM,
-						PedHash.MexBoss01GMM,
-						PedHash.MexBoss02GMM,
-						PedHash.OgBoss01AMM,
-						PedHash.SalvaBoss01GMY
-				};
+		{
+			PedHash.ChiBoss01GMM,
+			PedHash.KorBoss01GMM,
+			PedHash.MexBoss01GMM,
+			PedHash.MexBoss02GMM,
+			PedHash.OgBoss01AMM,
+			PedHash.SalvaBoss01GMY
+		};
 
+		/// <summary>
+		/// The <see cref="DrugDealerWeaponHashes"/> list holds all the possible dealer weapons
+		/// </summary>
 		public static readonly List<WeaponHash> DrugDealerWeaponHashes = new()
-				{
-						WeaponHash.Pistol,
-						WeaponHash.SMG,
-						WeaponHash.AssaultRifle,
-						WeaponHash.SawnOffShotgun,
-						WeaponHash.MachinePistol
-				};
+		{
+			WeaponHash.Pistol,
+			WeaponHash.SMG,
+			WeaponHash.AssaultRifle,
+			WeaponHash.SawnOffShotgun,
+			WeaponHash.MachinePistol
+		};
 
+		/// <summary>
+		/// The <see cref="DrugLordWeaponHashes"/> list holds all the possible drug lord weapons
+		/// </summary>
 		public static readonly List<WeaponHash> DrugLordWeaponHashes = new()
-				{
-						WeaponHash.HeavyPistol,
-						WeaponHash.AssaultSMG,
-						WeaponHash.SpecialCarbine,
-						WeaponHash.AssaultShotgun,
-						WeaponHash.CombatMG
-				};
+		{
+			WeaponHash.HeavyPistol,
+			WeaponHash.AssaultSMG,
+			WeaponHash.SpecialCarbine,
+			WeaponHash.AssaultShotgun,
+			WeaponHash.CombatMG
+		};
 
+		/// <summary>
+		/// The <see cref="DrugDealerSpawnLocations"/> list holds all the possible dealer spawn locations
+		/// </summary>
 		public static readonly Tuple<Vector3, float>[] DrugDealerSpawnLocations =
 		{
-						Tuple.Create(new Vector3(287.011f, -991.685f, 33.108f), 141.753f),
-						Tuple.Create(new Vector3(1095.781f, -2158.615f, 31.32f), 356.712f),
-						Tuple.Create(new Vector3(1136.588f, -1356.314f, 34.582f), 184.119f),
-						Tuple.Create(new Vector3(-624.057f, -1609.615f, 26.901f), 358.93f),
-						Tuple.Create(new Vector3(-71.055f, -1205.16f, 27.823f), 333.899f),
-						Tuple.Create(new Vector3(-1175.357f, -1571.212f, 4.357f), 155.562f),
-						Tuple.Create(new Vector3(895.643f, 3612.164f, 32.824f), 229.007f),
-						Tuple.Create(new Vector3(2442.651f, 4959.362f, 45.842f), 259.245f),
-						Tuple.Create(new Vector3(-283.2f, 6176.041f, 31.496f), 131.388f),
-						Tuple.Create(new Vector3(144.781f, -2407.616f, 6.001f), 261.199f),
-						Tuple.Create(new Vector3(-1462.811f, -368.759f, 39.635f), 160.347f),
-						Tuple.Create(new Vector3(-481.178f, -59.29f, 39.994f), 85.202f),
-						Tuple.Create(new Vector3(254.855f, -302.236f, 49.646f), 210.328f),
-						Tuple.Create(new Vector3(737.784f, 1195.371f, 326.265f), 335.606f),
-						Tuple.Create(new Vector3(-1837.719f, 791.407f, 138.696f), 119.483f),
-						Tuple.Create(new Vector3(551.738f, 2663.721f, 45.873f), 267.741f),
-						Tuple.Create(new Vector3(2405.682f, 3128.587f, 48.154f), 306.136f),
-						Tuple.Create(new Vector3(1469.889f, 6550.403f, 14.904f), 356.061f),
-						Tuple.Create(new Vector3(-141.62f, -1653.69f, 32.61f), 51.45f),
-						Tuple.Create(new Vector3(2544.97f, 364.42f, 108.61f), 88.92f),
-						Tuple.Create(new Vector3(-2185.1f, 4247.77f, 48.52f), 202.22f),
-						Tuple.Create(new Vector3(1491.33f, 1044.3f, 114.33f), 269.84f),
-						Tuple.Create(new Vector3(-37.29f, 1893.6f, 195.36f), 310.6f),
-						Tuple.Create(new Vector3(-1101.17f, 2723.73f, 18.8f), 28.03f),
-						Tuple.Create(new Vector3(85.63f, -1959.49f, 21.12f), 328.65f),
-						Tuple.Create(new Vector3(-1512.28f, 1520.81f, 115.29f), 80.55f),
-						Tuple.Create(new Vector3(1466.09f, -1930.76f, 71.32f), 149.76f),
-						Tuple.Create(new Vector3(342.3f, -2075.3f, 20.94f), 189.02f),
-						Tuple.Create(new Vector3(-464.73f, 5345.44f, 80.72f), 98.61f),
-						Tuple.Create(new Vector3(-693.68f, -751.71f, 25.04f), -166.03f)
-				};
-		//todo just dummy!
+			Tuple.Create(new Vector3(287.011f, -991.685f, 33.108f), 141.753f),
+			Tuple.Create(new Vector3(1095.781f, -2158.615f, 31.32f), 356.712f),
+			Tuple.Create(new Vector3(1136.588f, -1356.314f, 34.582f), 184.119f),
+			Tuple.Create(new Vector3(-624.057f, -1609.615f, 26.901f), 358.93f),
+			Tuple.Create(new Vector3(-71.055f, -1205.16f, 27.823f), 333.899f),
+			Tuple.Create(new Vector3(-1175.357f, -1571.212f, 4.357f), 155.562f),
+			Tuple.Create(new Vector3(895.643f, 3612.164f, 32.824f), 229.007f),
+			Tuple.Create(new Vector3(2442.651f, 4959.362f, 45.842f), 259.245f),
+			Tuple.Create(new Vector3(-283.2f, 6176.041f, 31.496f), 131.388f),
+			Tuple.Create(new Vector3(144.781f, -2407.616f, 6.001f), 261.199f),
+			Tuple.Create(new Vector3(-1462.811f, -368.759f, 39.635f), 160.347f),
+			Tuple.Create(new Vector3(-481.178f, -59.29f, 39.994f), 85.202f),
+			Tuple.Create(new Vector3(254.855f, -302.236f, 49.646f), 210.328f),
+			Tuple.Create(new Vector3(737.784f, 1195.371f, 326.265f), 335.606f),
+			Tuple.Create(new Vector3(-1837.719f, 791.407f, 138.696f), 119.483f),
+			Tuple.Create(new Vector3(551.738f, 2663.721f, 45.873f), 267.741f),
+			Tuple.Create(new Vector3(2405.682f, 3128.587f, 48.154f), 306.136f),
+			Tuple.Create(new Vector3(1469.889f, 6550.403f, 14.904f), 356.061f),
+			Tuple.Create(new Vector3(-141.62f, -1653.69f, 32.61f), 51.45f),
+			Tuple.Create(new Vector3(2544.97f, 364.42f, 108.61f), 88.92f),
+			Tuple.Create(new Vector3(-2185.1f, 4247.77f, 48.52f), 202.22f),
+			Tuple.Create(new Vector3(1491.33f, 1044.3f, 114.33f), 269.84f),
+			Tuple.Create(new Vector3(-37.29f, 1893.6f, 195.36f), 310.6f),
+			Tuple.Create(new Vector3(-1101.17f, 2723.73f, 18.8f), 28.03f),
+			Tuple.Create(new Vector3(85.63f, -1959.49f, 21.12f), 328.65f),
+			Tuple.Create(new Vector3(-1512.28f, 1520.81f, 115.29f), 80.55f),
+			Tuple.Create(new Vector3(1466.09f, -1930.76f, 71.32f), 149.76f),
+			Tuple.Create(new Vector3(342.3f, -2075.3f, 20.94f), 189.02f),
+			Tuple.Create(new Vector3(-464.73f, 5345.44f, 80.72f), 98.61f),
+			Tuple.Create(new Vector3(-693.68f, -751.71f, 25.04f), -166.03f)
+			};
+
+		/// <summary>
+		/// The <see cref="DrugLordSpawnLocations"/> list holds all the possible drug lord spawn locations
+		/// </summary>
+		//todo just dummy!		
 		public static readonly Tuple<Vector3, float>[] DrugLordSpawnLocations =
 		{
-						Tuple.Create(new Vector3(287.011f, -991.685f, 33.108f), 141.753f),
-						Tuple.Create(new Vector3(1095.781f, -2158.615f, 31.32f), 356.712f),
-						Tuple.Create(new Vector3(1136.588f, -1356.314f, 34.582f), 184.119f),
-						Tuple.Create(new Vector3(-624.057f, -1609.615f, 26.901f), 358.93f),
-						Tuple.Create(new Vector3(-71.055f, -1205.16f, 27.823f), 333.899f),
-				};
-
-		public static readonly List<DrugDealer> DrugDealers = new()
-				{
-						new DrugDealer(new Vector3(287.011f, -991.685f, 33.108f), 141.753f),
-						new DrugDealer(new Vector3(1095.781f, -2158.615f, 31.32f), 356.712f),
-						new DrugDealer(new Vector3(1136.588f, -1356.314f, 34.582f), 184.119f),
-						new DrugDealer(new Vector3(-624.057f, -1609.615f, 26.901f), 358.93f),
-						new DrugDealer(new Vector3(-71.055f, -1205.16f, 27.823f), 333.899f),
-						new DrugDealer(new Vector3(-1175.357f, -1571.212f, 4.357f), 155.562f),
-						new DrugDealer(new Vector3(895.643f, 3612.164f, 32.824f), 229.007f),
-						new DrugDealer(new Vector3(2442.651f, 4959.362f, 45.842f), 259.245f),
-						new DrugDealer(new Vector3(-283.2f, 6176.041f, 31.496f), 131.388f),
-						new DrugDealer(new Vector3(144.781f, -2407.616f, 6.001f), 261.199f),
-						new DrugDealer(new Vector3(-1462.811f, -368.759f, 39.635f), 160.347f),
-						new DrugDealer(new Vector3(-481.178f, -59.29f, 39.994f), 85.202f),
-						new DrugDealer(new Vector3(254.855f, -302.236f, 49.646f), 210.328f),
-						new DrugDealer(new Vector3(737.784f, 1195.371f, 326.265f), 335.606f),
-						new DrugDealer(new Vector3(-1837.719f, 791.407f, 138.696f), 119.483f),
-						new DrugDealer(new Vector3(551.738f, 2663.721f, 45.873f), 267.741f),
-						new DrugDealer(new Vector3(2405.682f, 3128.587f, 48.154f), 306.136f),
-						new DrugDealer(new Vector3(1469.889f, 6550.403f, 14.904f), 356.061f),
-						new DrugDealer(new Vector3(-141.62f, -1653.69f, 32.61f), 51.45f),
-						new DrugDealer(new Vector3(2544.97f, 364.42f, 108.61f), 88.92f),
-						new DrugDealer(new Vector3(-2185.1f, 4247.77f, 48.52f), 202.22f),
-						new DrugDealer(new Vector3(1491.33f, 1044.3f, 114.33f), 269.84f),
-						new DrugDealer(new Vector3(-37.29f, 1893.6f, 195.36f), 310.6f),
-						new DrugDealer(new Vector3(-1101.17f, 2723.73f, 18.8f), 28.03f),
-						new DrugDealer(new Vector3(85.63f, -1959.49f, 21.12f), 328.65f),
-						new DrugDealer(new Vector3(-1512.28f, 1520.81f, 115.29f), 80.55f),
-						new DrugDealer(new Vector3(1466.09f, -1930.76f, 71.32f), 149.76f),
-						new DrugDealer(new Vector3(342.3f, -2075.3f, 20.94f), 189.02f),
-						new DrugDealer(new Vector3(-464.73f, 5345.44f, 80.72f), 98.61f),
-						new DrugDealer(new Vector3(-693.68f, -751.71f, 25.04f), -166.03f)
-				};
+			Tuple.Create(new Vector3(287.011f, -991.685f, 33.108f), 141.753f),
+			Tuple.Create(new Vector3(1095.781f, -2158.615f, 31.32f), 356.712f),
+			Tuple.Create(new Vector3(1136.588f, -1356.314f, 34.582f), 184.119f),
+			Tuple.Create(new Vector3(-624.057f, -1609.615f, 26.901f), 358.93f),
+			Tuple.Create(new Vector3(-71.055f, -1205.16f, 27.823f), 333.899f),
+		};
 
 		public const float MarkerDrawDistance = 20f;
 
