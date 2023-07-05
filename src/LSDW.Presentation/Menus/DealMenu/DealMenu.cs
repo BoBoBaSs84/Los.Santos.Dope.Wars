274f88e13da93523a92c9b0993f9f5dde7211277
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2023-07-05 15:25:08 +0200

    test that ...

--- src/LSDW.Presentation/Menus/DealMenu/DealMenu.cs
+++ src/LSDW.Presentation/Menus/DealMenu/DealMenu.cs
@@ -1,39 +1,20 @@
 ﻿using LemonUI.Menus;
 using LSDW.Abstractions.Domain.Models;
-using LSDW.Abstractions.Domain.Providers;
 using LSDW.Abstractions.Extensions;
 using LSDW.Domain.Extensions;
-using LSDW.Domain.Factories;
 using LSDW.Presentation.Menus.Base;
 
 namespace LSDW.Presentation.Menus.DealMenu;
 public sealed class DealMenu : MenuBase
 {
-	private readonly INotificationProvider _notificationProvider = DomainFactory.GetNotificationProvider();
-
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="drugs"></param>
 	public DealMenu(ICollection<IDrug> drugs) : base("Buy", "Let's buy some drugs...")
 	{
 		foreach (IDrug drug in drugs)
-		{
-			NativeListItem<int> item = AddDrugListItem(drug, OnItemActivated);
-			item.Tag = drug;
-			item.SelectedIndex = drug.Quantity;
-			item.ItemChanged += (sender, args) => OnItemChanged(item, args);
-		}
-	}
-
-	private void OnItemActivated(NativeListItem<int> item, EventArgs args)
-	{
-		_notificationProvider.Show($"{nameof(OnItemActivated)}");
-		_notificationProvider.Show($"Title:\t{item.Title}");
-		_notificationProvider.Show($"Description:\t{item.Description}");
-		_notificationProvider.Show($"SelectedItem:\t{item.SelectedItem}");
-	}
-
-	private void OnItemChanged(NativeListItem<int> item, ItemChangedEventArgs<int> args)
-	{
-		_notificationProvider.Show($"{nameof(OnItemChanged)}");
-		_notificationProvider.Show($"SelectedItem:\t{item.SelectedItem}");
+			_ = AddDrugListItem(drug, OnItemActivated);
 	}
 
 	/// <summary>
@@ -43,5 +24,28 @@ public sealed class DealMenu : MenuBase
 	/// <param name="activated">The action to perform when activated.</param>
 	/// <param name="changed">The action to perform when the selected item of the list changes.</param>
 	private NativeListItem<int> AddDrugListItem(IDrug drug, Action<NativeListItem<int>, EventArgs>? activated = null, Action<NativeListItem<int>, ItemChangedEventArgs<int>>? changed = null)
-		=> AddListItem(drug.Type.GetDrugName(), drug.Type.GetDrugDescription(), activated, changed, drug.Quantity.GetArray());
+	{
+		NativeListItem<int> item = AddListItem(drug.Type.GetDrugName(), string.Empty, activated, changed, drug.Quantity.GetArray());
+		item.Enabled = drug.Quantity > 0;
+		item.Tag = drug;
+		item.SelectedIndex = drug.Quantity;
+		item.ItemChanged += (sender, args) => OnItemChanged(item, args);
+		return item;
+	}
+
+	private void OnItemActivated(NativeListItem<int> item, EventArgs args)
+	{
+		if (item.Tag is not IDrug drug)
+			return;
+		int quantityToMove = item.SelectedItem;
+
+		drug.Remove(quantityToMove);
+		item = AddDrugListItem(drug, OnItemActivated);
+	}
+
+	private void OnItemChanged(NativeListItem<int> item, ItemChangedEventArgs<int> args)
+	{
+		if (item.Tag is not IDrug drug)
+			return;
+	}
 }
