d7eaffd26a807a6e9aa3f223ccde7cdc463bcaf5
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-08-24 17:20:13 +0200

    feat: diversity male and female dealers
    changes:
    - tracfficking extension moodified
    - name constants modified
    - bedestrian base class modified
    - unit test added
    
    closes #218

--- src/LSDW.Domain/Constants/NameConstants.cs
+++ src/LSDW.Domain/Constants/NameConstants.cs
@@ -7,33 +7,49 @@ namespace LSDW.Domain.Constants;
 /// </summary>
 public static class NameConstants
 {
+	private static readonly string[] maleFirstNames = new string[]
+	{"Simon","John","Jack","Grant","Sandeep","Sylvester","Lionel","James","Garrett","Reinout","Eric","Mihail","Jan","George","Jeffrey","Kevin","Paul","Hazem","David","Tete","Zainal","François","Michael","Jack","William","Alejandro","Tawana","Willis","Jason","Hung-Fu","Michael","Stuart","Min","Rajesh","Chris","Gabe","Russell","John","Ken","Magnus","Thierry","Andreas","Pete","Ken","Zheng","Stuart","Lolan","Baris","Frank","Brian","David","Tengiz","John","Eric","Brandon","Michael","Jose","Kok-Ho","Ben","Chris","Ovidiu","Ebru","Rob","Sean","Vidur","Benjamin","Paul","Suroor","Taylor","David","David","Dragan","Bob","Chad","Michael","Pat","Karan","Shane","Sameer","Don"};
+	
+	private static readonly string[] lastNames = new string[]
+	{"Hillmann","Richins","Norman","King","Scardelis","Campbell","Duffy","Frintu","Okelberry","Smith","Mohamed","Abercrombie","Kaliyath","Rothkugel","Mares","Mirchandani","Sam","Ellerbrock","Petculescu","Cetinok","Hite","Gibson","Yukish","Dyck","Randall","McAskill-White","Poland","Johnson","Wilson","Hagens","Keil","Song","Keyser","Cracium","Koch","Nartker","Preston","Berge","Ito","Saraiva","Hall","Raheem","Harnpadoungsataya","Zimmerman","Rettig","Hamilton","Martin","Valdez","Dobney","Culbertson","Kleinerman","Laszlo","Gubbels","Sousa","Walters","Koenigsbauer","Trenary","Tsoflias","Reátegui Alayo","Dempsey","Benshoof","Munson","Connelly","Vande Velde","Miller","Singh","Johnson","Erickson","Hay","Sunkammurali","Ting","Randall","Barbariol","Ingle","Baker","Adams","Meyyappan","Northup","Bradley","Martin"};
+	
+	private static readonly string[] femaleFirstNames = new string[]
+	{"Karen","Kitti","Barbara","Jo","Merav","Alice","Margie","Jae","Linda","Belinda","Carol","Janeth","Kathie","Kim","Sandra","Diane","Barbara","Linda","Kimberly","Katie","Samantha","Yvonne","Erin","Shelley","Paula","Lynn","Britta","Doris","Rebecca","Wendy","Cynthia","Annette","Laura","Wanida","Elizabeth","Denise","Jill","Betsy","Susan","Jo","Stephanie","Paula","Ruth","Gigi","Linda","Nancy","Laura","Lori","Lorraine","Jillian","Lori","Susan","Linda","Carole","Pamela","Candy","Mary","JoLynn","Kim","Amy","Janet","Deborah","Rachel","Sharon","Anibal","Terri","Diane","Sheela","Mary","Olinda","Diane","Mary","Nicole","Karen","Gail","Bonnie","Danielle","Mindy","Suchitra","Brenda","Janice","Angela","Janaina","Jean"};
+	
 	/// <summary>
-	/// The first names string array.
+	/// Returns a randomly chossen male first name.
 	/// </summary>
-	private static readonly string[] FirstNames = new string[]
-	{"Abel","Abraham","Adam","Adolfo","Ahmed","Alan","Alden","Aldo","Alex","Alexis","Alonso","Alonzo","Alvin","Ambrose","Andre","Andreas","Anton","Antwan","Armand","Armando","Arnold","Arron","August","Barrett","Bart","Basil","Benny","Bert","Blair","Bo","Bobbie","Branden","Brenton","Brooks","Bruce","Byron","Caleb","Cameron","Carl","Cary","Chad","Chang","Charles","Charley","Chas","Chester","Chi","Christian","Christopher","Chuck","Clark","Clifton","Clyde","Cody","Coleman","Columbus","Connie","Cornelius","Courtney","Cristopher","Cruz","Curtis","Dallas","Dalton","Dane","Danny","Daren","Darrell","Darren","Darron","Darryl","David","Davis","Deandre","Dennis","Deon","Devon","Dewitt","Dexter","Dillon","Dino","Dirk","Dominic","Dominick","Don","Donald","Donovan","Dorsey","Doug","Douglass","Doyle","Drew","Dustin","Earnest","Ed","Eddy","Edmund","Efrain","Efren","Elbert","Elisha","Ellis","Elvin","Emanuel","Emerson","Emery","Emile","Emilio","Emmett","Erich","Erik","Erin","Ernie","Ervin","Erwin","Eugene","Eusebio","Fausto","Federico","Fermin","Florencio","Francis","Fred","Freddie","Frederick","Fredric","Gail","Garfield","Garland","Garrett","Gary","Gayle","Gaylord","Gene","Geoffrey","George","Gil","Gilbert","Gonzalo","Gordon","Grady","Graham","Guillermo","Hai","Harrison","Harry","Herbert","Hilario","Homer","Horace","Horacio","Houston","Howard","Hung","Ike","Irving","Irwin","Isaiah","Isaias","Ismael","Ivan","Jacinto","Jaime","Jake","Jamal","Jame","James","Jarred","Jarrett","Jarrod","Jasper","Jefferson","Jeffry","Jerald","Jere","Jeremy","Jerome","Jerrold","Jess","Jesse","Jessie","Jim","Jimmy","Joaquin","Joe","Johnie","Jonah","Jonas","Jonathan","Jordan","Josef","Joshua","Jospeh","Juan","Julian","Julius","Karl","Keenan","Ken","Kendall","Kenton","Kerry","Kirby","Kris","Kristopher","Kurt","Landon","Lane","Lanny","Lauren","Laverne","Lawerence","Lawrence","Lazaro","Lee","Lemuel","Len","Leo","Leonel","Les","Lesley","Levi","Lindsay","Linwood","Lloyd","Lonnie","Loren","Lorenzo","Lowell","Loyd","Luciano","Luigi","Luis","Lynn","Malcolm","Manuel","Marcelo","Marcus","Mario","Marshall","Martin","Mary","Matthew","Maurice","Max","Micah","Michal","Mickey","Milton","Minh","Mitch","Mitchell","Mohamed","Morris","Moses","Murray","Napoleon","Nathaniel","Nelson","Nestor","Neville","Newton","Nicholas","Nigel","Noe","Noel","Norbert","Norberto","Ollie","Omar","Omer","Osvaldo","Oswaldo","Palmer","Parker","Pat","Pete","Phillip","Quincy","Quinton","Rafael","Ralph","Ramon","Raphael","Rashad","Ray","Raymundo","Refugio","Rey","Reyes","Ricardo","Rick","Ricky","Rigoberto","Rob","Robert","Roberto","Roderick","Rodolfo","Roland","Rolando","Ronald","Ronnie","Roosevelt","Rosario","Ross","Rufus","Russell","Sammy","Santos","Scot","Sebastian","Seymour","Shad","Shane","Shannon","Shaun","Shelby","Shelton","Shirley","Shon","Stanford","Steven","Stevie","Sung","Taylor","Teddy","Terence","Terry","Theodore","Theron","Thomas","Thurman","Tim","Timothy","Titus","Tobias","Toby","Tod","Todd","Tom","Tommy","Toney","Tony","Tracey","Travis","Trent","Troy","Tyler","Ulysses","Valentine","Van","Vern","Vernon","Vicente","Victor","Vince","Wade","Walker","Wallace","Wally","Waylon","Wayne","Weldon","Wendell","Weston","Wilbert","William","Wyatt","Zachary","Zachery"};
+	public static string GetMaleFirstName()
+		=> maleFirstNames[RandomHelper.GetInt(default, maleFirstNames.Length)];
 
 	/// <summary>
-	/// The last names string array.
+	/// Returns a randomly chossen female first name.
 	/// </summary>
-	private static readonly string[] LastNames = new string[]
-	{"Abel","Adkins","Aguiar","Akins","Alley","Alvarez","Ambrose","Andrade","Ashcraft","August","Austin","Bagley","Baker","Baldwin","Barela","Barraza","Barrett","Barrows","Barton","Beaver","Behrens","Bell","Benavides","Bennett","Bishop","Blair","Blake","Blakely","Blount","Bobbitt","Bock","Bolin","Bollinger","Boone","Bostic","Boswell","Bourgeois","Bowen","Bowers","Brenner","Bridges","Brill","Brittain","Britton","Bui","Bundy","Burk","Burke","Burns","Burton","Button","Byrd","Campbell","Caraballo","Carbone","Carmichael","Carroll","Castillo","Castro","Chapman","Clark","Cleveland","Cobb","Cody","Collier","Colon","Compton","Cooks","Cooper","Copeland","Cornwell","Craig","Crist","Custer","Damico","Daniels","Darby","Dasilva","Davis","Devore","Devries","Dewey","Dion","Dowd","Dowdy","Downs","Doyle","Drew","Dudley","Dunn","Dunning","Duval","Early","Eddy","Edwards","Elam","Elliott","Ellis","Engel","Erickson","Espinosa","Estrada","Evans","Faison","Farmer","Farrell","Felton","Fernandes","Ferrara","Fisher","Fitch","Flannery","Fleming","Foley","Folse","Ford","Forsyth","Fountain","Francis","Franco","Frank","Franklin","Frazier","French","Fuller","Gable","Gabriel","Gale","Gallegos","Garcia","Gardner","Garrett","Garris","Gee","Gibson","Gilbert","Gill","Gillis","Gilmore","Ginn","Girard","Givens","Glass","Gonzales","Goode","Gooden","Goodman","Goodwin","Graf","Greene","Greer","Grove","Hahn","Hale","Hammond","Hampton","Hardin","Hardy","Harrison","Hart","Hayes","Haynes","Hazel","Headrick","Heinz","Henderson","Henry","Herrera","Herrington","Hickson","Higgins","Hodges","Hogan","Holley","Hopper","Houghton","House","Howard","Huff","Huffman","Hunt","Hunter","Huntley","Jacobs","Jean","Jefferson","Jennings","Jimenez","Johnson","Johnston","Kelley","Kennedy","Killian","Kimball","Knight","Knowlton","Knox","Kozlowski","Kurtz","Lachance","Lamb","Lane","Lau","Lawrence","Lay","Lemon","Leonard","Levy","Lindquist","Lindsey","Linkous","Lloyd","Lombard","Longoria","Lopez","Love","Lujan","Macias","Mackenzie","Maclean","Magee","Maldonado","Mann","Marquis","May","Mayer","Mcconnell","Mcguire","Mckenzie","Mcpherson","Medina","Meek","Mendoza","Metzler","Meyer","Michel","Miller","Mills","Milne","Mobley","Monroe","Montero","Mora","Morales","Morris","Moss","Mullis","Munoz","Murphy","Murray","Myers","Nash","Newton","Ng","Nichols","Noriega","Norman","Norton","Nunez","Obrien","Oconner","Otoole","Owen","Pantoja","Parsons","Patino","Patrick","Patton","Peachey","Pena","Pence","Pennington","Perry","Person","Peters","Phelps","Pierce","Pitre","Poe","Pope","Posey","Powell","Presley","Pritchard","Quinlan","Rafferty","Ragsdale","Ramirez","Ramsey","Raynor","Reese","Reyes","Rhodes","Richards","Richardson","Riley","Ritter","Roberts","Robinson","Rodgers","Roldan","Rowland","Roy","Salgado","Sammons","Samples","Sanchez","Sands","Santana","Santiago","Seay","Serna","Shank","Shelton","Sherrod","Simmons","Simon","Sinclair","Singleton","Slaughter","Smiley","Smith","Soto","Southerland","Spaulding","Stanley","Steele","Stewart","Stine","Sullivan","Sutherland","Sutton","Swafford","Taber","Tate","Tatum","Tejeda","Thorne","Thornhill","Thornton","Tiller","Titus","Todd","Torres","Tovar","Townsend","Tran","Turnbull","Turpin","Utley","Vega","Vines","Vitale","Wagner","Wakefield","Walker","Wallen","Walsh","Watkins","Watson","Wharton","Wild","Williamson","Wilson","Wise","Woods","Woody","Worthy","Wright","Wyman","Yates","Young","Zeigler"};
-
-	/// <summary>
-	/// Returns a randomly chossen first name.
-	/// </summary>
-	public static string GetFirstName()
-		=> FirstNames[RandomHelper.GetInt(default, FirstNames.Length)];
+	public static string GetFemaleFirstName()
+		=> maleFirstNames[RandomHelper.GetInt(default, femaleFirstNames.Length)];
 
 	/// <summary>
 	/// Returns a randomly chossen last name.
 	/// </summary>
 	public static string GetLastName()
-		=> LastNames[RandomHelper.GetInt(default, LastNames.Length)];
+		=> lastNames[RandomHelper.GetInt(default, lastNames.Length)];
 
 	/// <summary>
-	/// Returns a randomly chossen first and last name.
+	/// Returns a randomly chossen male first name and last name.
 	/// </summary>
-	public static string GetFullName()
-		=> string.Concat(GetFirstName(), " ", GetLastName());
+	public static string GetMaleFullName()
+		=> string.Concat(GetMaleFirstName(), " ", GetLastName());
+
+	/// <summary>
+	/// Returns a randomly chossen female first name and last name.
+	/// </summary>
+	public static string GetFemaleFullName()
+		=> string.Concat(GetFemaleFirstName(), " ", GetLastName());
+
+	/// <summary>
+	/// Returns a randomly chossen male or female first name and last name
+	/// </summary>
+	/// <param name="female"><see langword="true"/> if a female full name should be returned.</param>
+	public static string GetFullName(bool female)
+		=> female ? GetFemaleFullName() : GetMaleFullName();
 }
